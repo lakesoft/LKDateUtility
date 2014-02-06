97c606fc14fb44ea07eb8ac437ca06ebc070ade7
Hiroshi Hashiguchi <hashiguchi@lakesoft.jp>
2014-02-06 12:30:36 +0900

    Initial Commit

---
+++ LKDateUtility/LKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LKAppDelegate.h
+//  LKDateUtility
+//
+//  Created by Hiroshi Hashiguchi on 2014/02/06.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
